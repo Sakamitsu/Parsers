b5adea2ebbd29cd2fb5da81494a35fc0a61c110a
Sakamitsu <86984855+Sakamitsu@users.noreply.github.com>
2022-03-15 17:47:59 +0300

    Add project files.

---
+++ NmarketTestTask/Parsers/ExcelParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClosedXML.Excel;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class ExcelParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            IList<House> list = new List<House>();
+            List<int> uniqueHouses = new List<int>();
+            var workbook = new XLWorkbook(path);
+            var rows = workbook.Worksheet(1).RangeUsed().RowsUsed();
+
+            var cells = rows.Cells().Where(c => c.GetString().Contains("№")).ToList();
+            var houses = rows.Cells().Where(c => c.GetString().Contains("Дом")).ToList();
+
+
+            foreach (var houseName in houses)
+            {
+                House house = new House();
+                house.Name = houseName.GetString();
+                uniqueHouses.Add(houseName.Address.RowNumber);
+                list.Add(house);
+
+            }
+
+
+            foreach (var cell in cells)
+            {
+                string flatNumber = cell.GetString().Replace("№", string.Empty);
+                string flatPrice = cell.CellBelow().GetString();
+                Flat flat = new Flat();
+                flat.Number = flatNumber;
+                flat.Price = flatPrice;
+                for (int i = 0; i < uniqueHouses.Count; i++)
+                {
+                    try
+                    {
+                        if (cell.Address.RowNumber > uniqueHouses[i] && cell.Address.RowNumber < uniqueHouses[i + 1])
+                        {
+                            list[i].Flats.Add(flat);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        list[i].Flats.Add(flat);
+                    }
+                    
+                }
+                
+            }
+            return list;
+        }
+    }
+}
---
+++ NmarketTestTask/Parsers/HtmlParser.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class HtmlParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            IList<House> list = new List<House>();
+            List<string> uniqueHouses = new List<string>();
+
+            var doc = new HtmlDocument();
+            doc.Load(path);
+            var table = doc.DocumentNode.SelectSingleNode("//tbody");
+            var rows = table.SelectNodes(".//tr");
+            foreach (var row in rows)
+            {
+                string houseNumber = row.SelectSingleNode(".//td[@class='house']").InnerText;
+                string flatNumber = row.SelectSingleNode(".//td[@class='number']").InnerText;
+                string flatPrice = row.SelectSingleNode(".//td[@class='price']").InnerText;
+
+                if (uniqueHouses.Distinct().ToList().Contains(houseNumber))
+                {
+                    foreach (var house in list)
+                    {
+                        if (house.Name == houseNumber)
+                        {
+                            Flat flat = new Flat();
+                            flat.Number = flatNumber;
+                            flat.Price = flatPrice;
+                            house.Flats.Add(flat);
+                        }
+                    }
+                }
+                else
+                {
+                    House house = new House();
+                    Flat flat = new Flat();
+                    house.Name = houseNumber;
+                    flat.Number = flatNumber;
+                    flat.Price = flatPrice;
+                    house.Flats.Add(flat);
+                    list.Add(house);
+                }
+                uniqueHouses.Add(houseNumber);
+            }
+            return list;
+        }
+    }
+}
